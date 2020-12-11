a582dae759b0ee3071d76d0ce948ba09fce6654a
akrsh24 <akrsh24@gmail.com>
2020-12-11 23:49:50 +0530

    Added closure files

--- hoisting/index.js
+++ hoisting/index.js
@@ -4,34 +4,34 @@
 //2. It's an application of execution contexts.
 
 //--------------------
-function number(num) {
-    return num;
-}
+// function number(num) {
+//     return num;
+// }
 
-console.log(a);
-var a = 5;
-console.log(a);
+// console.log(a);
+// var a = 5;
+// console.log(b);
 
 // Here, according to execution context, memory creation phase will be executed at first, which will define and initialise a=undefined. And when we define it to a number(5), it get updated.
 // Hoisting is an application of execution context, where we see how execution context works.
 
 // var variables hold undefined without any error.
 // let variables hold undefined but gives reference error, as we cannot access any variable without initialisation.
-let b = 2;
-console.log(b);
+// let b = 2;
+// console.log(b);
 
 //--------------------
 
 
 //--------------------
-function square(num) {
-    // console.log("Args->",args);
-    var args = num;
-    console.log("Args->",args);
-}
+// function square(num) {
+//     // console.log("Args->",args);
+//     var args = num;
+//     console.log("Args->",args);
+// }
 
-square(5);
-console.log(args);
+// square(5);
+// console.log(args);
 
 //--------------------
 
