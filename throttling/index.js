e46e8468aadfc5b4aea216f20cb465a7f349b1af
Akarsh Srivastava <akrsh24@gmail.com>
2022-04-04 17:20:20 +0530

    Update index.js

--- throttling/index.js
+++ throttling/index.js
@@ -1,25 +1,57 @@
-let counter = 0;
-function getData() {
-    console.log("Throttling Event...", counter++);
-}
+// let counter = 0;
+// function getData() {
+//     console.log("Throttling Event...", counter++);
+// }
 
+// function throttling(fn, d) {
+//     let timer;
+//     return function () {
+//         let context = this,
+//             args = arguments;
+//         clearTimeout(timer);
+//         timer = setTimeout(() => {
+//             fn.apply(context, args)
+//         }, d);
+//     }
+// }
 
-function throttling(fn, d) {
-    let timer;
-    return function () {
-        let context = this,
-            args = arguments;
-        clearTimeout(timer);
-        timer = setTimeout(() => {
-            fn.apply(context, args)
-        }, d);
-    }
-}
+// const throttlingFunction = throttling(getData, 300);
 
-const throttlingFunction = throttling(getData, 300);
+// // dry run ->
 
+// // a-> b -> c- >d
 
+// // Throttling triggers after every particular interval time.
 
-// dry run ->
+const normalCount = document.querySelector("#api-count");
+const throttleCount = document.querySelector("#api-count-throttling");
+const inputDOM = document.querySelector("#name");
+let timerId;
 
-// a-> b -> c- >d 
+inputDOM.addEventListener("input", () => {
+  //normal count
+  let count = normalCount.innerHTML || 0;
+  count = makeAPICall(count);
+  normalCount.innerHTML = count;
+
+  // throttle count
+  throttle(makeAPICall, 1000);
+});
+
+const makeAPICall = function (count) {
+  count = parseInt(count) + 1;
+  return count;
+};
+
+const throttle = function (func, timeout) {
+  // do nothing if timerId already exists
+  if (timerId) return;
+
+  // timer to execute after every timeout
+  timerId = setTimeout(() => {
+    let count = throttleCount.innerHTML || 0;
+    count = func(count);
+    throttleCount.innerHTML = count;
+    timerId = undefined;
+  }, timeout);
+};
